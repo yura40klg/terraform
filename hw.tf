6c55e8227d2211bb78405c1bf91e0f617d7ed438
yura <y.everts@bitsl.ru>
2022-05-02 10:18:41 +0300

    ver1

--- hw.tf
+++ hw.tf
@@ -13,34 +13,12 @@ provider "yandex" {
   folder_id = "b1gra9c1ucqnfnmki3lj"
   zone      = "ru-central1-a"
 }
-resource "yandex_compute_instance" "vm1" {
-  name        = "dev"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-resources {
-    cores  = 2
-    memory = 4
-  }
- boot_disk {
-    initialize_params {
-      image_id = "fd8sc0f4358r8pt128gg"
-    }
-}
-network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
-resource "yandex_compute_instance" "vm2" {
-  name = "prod"
+resource "yandex_compute_instance" "vm-1" {
+  name = "dev"
 
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
 
   boot_disk {
@@ -49,14 +27,8 @@ resource "yandex_compute_instance" "vm2" {
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
-}
-
 }
