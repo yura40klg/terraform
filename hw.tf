21f6789737508f6052261f408c97ae3eb4c491c3
yura <y.everts@bitsl.ru>
2022-05-03 22:23:08 +0300

    ver1

--- hw.tf
+++ hw.tf
@@ -13,22 +13,67 @@ provider "yandex" {
   folder_id = "b1gra9c1ucqnfnmki3lj"
   zone      = "ru-central1-a"
 }
-resource "yandex_compute_instance" "vm-1" {
-  name = "dev"
+
+data "yandex_compute_image" "container-optimized-image" {
+  family = "container-optimized-image"
+}
+
+resource "yandex_compute_instance" "vm1" {
+ name = "dev"
 
   resources {
     cores  = 2
     memory = 2
   }
 
-  boot_disk {
+ boot_disk {
     initialize_params {
-      image_id = "fd8sc0f4358r8pt128gg"
+      image_id = data.yandex_compute_image.container-optimized-image.id
     }
   }
 
+network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+     nat       = true
+   }
 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    docker-container-declaration = file("decdev.yml")
   }
+}
+resource "yandex_compute_instance" "vm2" {
+ name = "prod"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+ boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.container-optimized-image.id
+    }
+  }
+
+network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+     nat       = true
+   }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    docker-container-declaration = file("decprod.yml")
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
